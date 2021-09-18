c5b7006fd501a6424bbfa7c4bdb5d2895ca2dc3a
Hyunsoo, Chung <admin@hyonsu.com>
2021-09-19 02:53:03 +0900

    fix: translated /ranking command

--- commands/ranking.js
+++ commands/ranking.js
@@ -3,7 +3,7 @@ const api = require('../api');
 module.exports = {
     info: {
         name: 'ranking',
-        description: 'PP 랭킹을 표시합니다. // translate plz'
+        description: 'PP 랭킹을 표시합니다. // Show PP ranking.'
     },
     handler: async interaction => {
         const ranking = await api.getPPEmbedField(interaction.user.id, interaction.channel);
