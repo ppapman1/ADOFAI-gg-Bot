2784bcfa7420f4c78088c9fd94ffa45490890de5
Hyunsoo, Chung <admin@hyonsu.com>
2021-09-21 04:47:56 +0900

    feat: /invite command

---
+++ commands/invite.js
@@ -0,0 +1,17 @@
+const lang = require('../lang');
+
+const setting = require('../setting.json');
+
+module.exports = {
+    info: {
+        name: 'invite',
+        description: '봇을 초대하는 링크를 표시합니다. // Show bot invite link.'
+    },
+    handler: async interaction => {
+        return interaction.reply(
+            lang.langByLangName(interaction.dbUser.lang, 'BOT_INVITE')
+                .replace('{minperm}', `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=${setting.INVITE_PERMISSION}&scope=bot%20applications.commands`)
+                .replace('{admin}', `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot%20applications.commands`)
+        );
+    }
+}
