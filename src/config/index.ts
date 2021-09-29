585ec386bf458fa458968cbc01d972181c97665b
CrackThrough <crackthrough@gmail.com>
2021-09-29 15:28:03 +0900

    Add: Add config and Global

---
+++ src/config/index.ts
@@ -0,0 +1,29 @@
+import { JsonValue } from "../typings";
+import { FileData } from "../util";
+
+export class Config extends FileData {
+    protected static override _instance: Config;
+
+    /**
+     * Instance of this class.
+     */
+    static override get instance() {
+        return this._instance;
+    }
+
+    static override Load(): void {
+        this.LoadFrom("./dist/config");
+    }
+
+    // extended features
+    // -----------------
+
+    /**
+     * Gets configuration data using a key.
+     * @param key Key to find the config value from, syntax is `{flatParsedKey}`.
+     * @returns Config data in key
+     */
+    static override Get(key: string) {
+        return this.instance.data.get("config")?.get(key);
+    }
+}
