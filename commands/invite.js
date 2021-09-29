a6095f0e91380b66eae13db6a8968f6cbdb20117
Hyunsoo, Chung <admin@hyonsu.com>
2021-09-29 21:20:30 +0900

    fix: add BOT_PERMISSION setting

--- commands/invite.js
+++ commands/invite.js
@@ -10,7 +10,7 @@ module.exports = {
     handler: async interaction => {
         return interaction.reply(
             lang.langByLangName(interaction.dbUser.lang, 'BOT_INVITE')
-                .replace('{minperm}', `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=${setting.INVITE_PERMISSION}&scope=bot%20applications.commands`)
+                .replace('{minperm}', `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=${setting.BOT_PERMISSION}&scope=bot%20applications.commands`)
                 .replace('{admin}', `https://discord.com/api/oauth2/authorize?client_id=${interaction.client.user.id}&permissions=8&scope=bot%20applications.commands`)
         );
     }
