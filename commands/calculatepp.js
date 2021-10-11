b8c304ab7c6eb1e4b7ef15023b9dbd04b8346a15
yyy123454321 <91386838+yyy123454321@users.noreply.github.com>
2021-10-11 15:24:32 +0900

    Update calculatepp.js

--- commands/calculatepp.js
+++ commands/calculatepp.js
@@ -1,1 +1,37 @@
+const api = require('../api');
+const lang = require("../lang");
 
+module.exports = {
+    info: {
+        name: 'calculatepp',
+        description: 'PP를 계산합니다. // Calculate PP',
+        options: [
+            {
+                name: 'id',
+                description: '레벨 ID입니다. // This is the level ID.',
+                type: 'NUMBER',
+                required: true
+            },
+            {
+                name: 'accuracy',
+                description: '정확도입니다. // This is accuracy.',
+                type: 'NUMBER'
+            },
+            {
+                name: 'pitch',
+                description: '피치입니다. // This is pitch.',
+                type: 'NUMBER'
+            }
+        ]
+    },
+    handler: async interaction => {
+        await interaction.deferReply();
+
+        const levelID = interaction.options.getNumber('id');
+        const level = await api.getLevel(levelID);
+
+        if(!level) return interaction.editReply(lang.langByLangName(interaction.dbUser.lang, 'LEVEL_NOT_FOUND'));
+
+        return interaction.editReply(api.getLevelInfoMessage(level, interaction.dbUser.lang));
+    }
+}
