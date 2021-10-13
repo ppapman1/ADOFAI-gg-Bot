d2a8fcfd6a273a3837d210bb44955583a5d8c101
DESKTOP-IGEK31A\acer <mcyyy123454321@gmail.com>
2021-10-14 00:49:00 +0900

    refactor: calculation part
    refactor: calculation part

--- commands/calculatepp.js
+++ commands/calculatepp.js
@@ -18,7 +18,7 @@ module.exports = {
             },
             {
                 name: 'accuracy',
-                description: '정확도입니다. // This is accuracy.',
+                description: '정확도입니다. // This is accuracy. ',
                 type: 'NUMBER'
             },
             {
@@ -33,17 +33,23 @@ module.exports = {
 
         const levelID = interaction.options.getNumber('id');
         const level = await api.getLevel(levelID);
-        const accuracy = interaction.options.getNumber('accuracy');
+        let accuracy = interaction.options.getNumber('accuracy');
+        const maxAccuracy = (100+(level.tiles*0.01));
         const pitch = interaction.options.getNumber('pitch') || 100;
-        
+            
+        //error
         if(!level) return interaction.editReply(lang.langByLangName(interaction.dbUser.lang, 'LEVEL_NOT_FOUND'));
         
+        if(accuracy) if(accuracy < 0) return interaction.editReply(lang.langByLangName(interaction.dbUser.lang, 'ACCURACY_IS_TOO_LOW'));
+            
+        if(pitch < 1) return interaction.editReply(lang.langByLangName(interaction.dbUser.lang, 'PITCH_IS_TOO_LOW'));
+        if(pitch > 100000) return interaction.editReply(lang.langByLangName(interaction.dbUser.lang, 'PITCH_IS_TOO_HIGH'));
         
-        const title = `${level.artists.join(' & ')} - ${level.title}`;
-
+        
+        //emoji
         let difficultyString = level.difficulty.toString();
         if(level.censored) difficultyString = 'minus2';
-
+    
         const levelEmoji = main.Server.emoji[difficultyString];
         if(!levelEmoji) return {
             content: lang.langByLangName(interaction.dbUser.lang, 'UNSUPPORTED_LEVEL'),
@@ -51,32 +57,68 @@ module.exports = {
         }
         
         
-        //calculate pp
-        let diffV2 = (level.difficulty == -1) ? 20.5 : level.difficulty;
-        let ratingByDiff = (diffV2 == -2) ? 0.0 : (1600/(1+Math.exp(-0.42*level.difficulty+7.4)));
+        //Calculate PP
+        let ratingByDiff, accuracyV2, magByTiles, magByAcc, magByPitch;
         
-        let magByTiles = (level.tiles < 2000) ? magByTiles = 0.84+level.tiles/12500 : (level.tiles/2000)^0.1;
+        //Rating by Difficulty
+        if(level.difficulty == -2) {
+            ratingByDiff = 0.0;
+        } else {
+            ratingByDiff = 1600/(1+Math.exp(-0.42*((level.difficulty == -1) ? 20.5 : level.difficulty)+7.4));
+        }
+        
+        //Magnification by Tiles
+        if(level.tiles < 2000) {
+            magByTiles = 0.84+level.tiles/12500;
+        } else {
+            magByTiles = (level.tiles/2000)^0.1;
+        }
 
-        let accuracyV2 = accuracy ? accuracy/(100+(level.tiles*0.01)) : 0.93;
-        let magByAcc = 0.013/(-(accuracyV2)+1.0125)+0.2;
+        //Converted Accuracy
+        if(!accuracy) {
+            accuracyV2 = 0.93;
+            accuracy = maxAccuracy*0.93;
+        } else if(accuracy > maxAccuracy) {
+            //최대 정확도를 넘으면 정확도를 내부에서 -1로 저장하여 사용함
+            accuracyV2 = 1.0;
+            accuracy = -1;
+        } else {
+            accuracyV2 = accuracy/maxAccuracy;
+        }
         
-        let magByPitch = (pitch < 100) ? Math.pow((pitch/100), 1.8) : Math.pow(((1+(pitch/100))/2),Math.min((0.1+(level.tiles)^0.5/(2000^0.5),1.1)));
+        //Magnification by Accuracy
+        magByAcc = 0.013/(-(accuracyV2)+1.0125)+0.2;
         
+        //Magnification by Pitch
+        if(pitch < 100) {
+            magByPitch = Math.pow((pitch/100), 1.8);
+        } else {
+            magByPitch = Math.pow(((1+(pitch/100))/2),Math.min((0.1+(level.tiles)^0.5/(2000^0.5),1.1)));
+        }
+        
+        //Final Rating
         const resultPP = Math.pow(ratingByDiff*magByAcc*magByPitch*magByTiles, 1.01);
         
-		return interaction.editReply({
-			embeds: [
-				new MessageEmbed()
-					.setColor('#349eeb')
-					.setTitle(title)
-					.setURL(`${setting.MAIN_SITE}/levels/${level.id}`)
-					.setDescription(lang.langByLangName(interaction.dbUser.lang, 'EXPECTED_PP').replace('{pp}',Math.pow(ratingByDiff*magByAcc*magByPitch*magByTiles, 1.01).toFixed(4).toString()) + "\n\u200B")
-					.addField('Lv.', levelEmoji.toString(), true)
-					.addField('Speed', "x" + (pitch/100).toString(), true)
-					.addField('Accuracy', ((accuracy) ? accuracy : ((100+(level.tiles*0.01))*0.93)).toString() + "%", true)
-					//.setImage(`https://i.ytimg.com/vi/${utils.parseYouTubeLink(level.video).videoCode}/original.jpg`)
-					.setFooter(`ID : ${level.id}`)
-			]
-        });
+        
+        //return Embed
+        const returnEmbed = new MessageEmbed()
+            .setColor('#349eeb')
+            .setTitle(`${level.artists.join(' & ')} - ${level.title}`)
+            .setURL(`${setting.MAIN_SITE}/levels/${level.id}`)
+            .setDescription(lang.langByLangName(interaction.dbUser.lang, 'EXPECTED_PP').replace('{pp}', resultPP.toFixed(2)) + "\n\u200B")
+            .addField('Lv.', levelEmoji.toString(), true)
+            .addField('Speed', "`×" + (pitch/100).toFixed(2) + "`", true)
+            .addField('Accuracy', "`" + ((accuracy == -1) ? maxAccuracy : accuracy).toFixed(2) + "%`", true)
+            //.setImage(`https://i.ytimg.com/vi/${utils.parseYouTubeLink(level.video).videoCode}/original.jpg`)
+            .setFooter(`ID : ${level.id}`);
+        
+        if(accuracy == -1) {
+            return interaction.editReply({
+                embeds: [returnEmbed],
+                content: lang.langByLangName(interaction.dbUser.lang, 'ACCURACY_CANNOT_EXCEED').replace("{accuracy}", maxAccuracy.toFixed(2))
+            });
+        } else {
+            return interaction.editReply({embeds: [returnEmbed]});
+        }
     }
 }
