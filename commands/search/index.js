1009e0fffa108b503c51e45d2ca0b5edfb1ef450
Hyunsoo, Chung <admin@hyonsu.com>
2021-10-20 21:04:00 +0900

    chore: remove required for query option

--- commands/search/index.js
+++ commands/search/index.js
@@ -15,8 +15,7 @@ module.exports = {
                     {
                         name: 'query',
                         description: '검색할 레벨의 이름, 제작자, 작곡가 등을 입력하세요. // Enter the name, or creator, or artist.',
-                        type: 'STRING',
-                        required: true
+                        type: 'STRING'
                     },
                     {
                         name: 'mindifficulty',
