d1dc462958ea0c07de199e0af53ecbd5ca8cb19f
Hyunsoo, Chung <admin@hyonsu.com>
2021-10-25 15:38:03 +0900

    chore: remove send dm on change ticket name

--- commands/ticket/setname.js
+++ commands/ticket/setname.js
@@ -15,18 +15,18 @@ module.exports = async interaction => {
         reason: `${interaction.user.username} changed ticket name`
     });
 
-    const ticketUser = await interaction.client.users.fetch(ticket.user);
-    if(ticketUser) try {
-        await ticketUser.send({
-            embeds: [
-                new MessageEmbed()
-                    .setColor('#00ff00')
-                    .setTitle('티켓 이름 변경 / Ticket Name Changed')
-                    .setDescription(`티켓 이름이 ${name}${utils.checkBatchim(name) ? '으' : ''}로 변경되었습니다.\nTicket name has been changed to ${name}.`)
-                    .setTimestamp()
-            ]
-        });
-    } catch(e) {}
+    // const ticketUser = await interaction.client.users.fetch(ticket.user);
+    // if(ticketUser) try {
+    //     await ticketUser.send({
+    //         embeds: [
+    //             new MessageEmbed()
+    //                 .setColor('#00ff00')
+    //                 .setTitle('티켓 이름 변경 / Ticket Name Changed')
+    //                 .setDescription(`티켓 이름이 ${name}${utils.checkBatchim(name) ? '으' : ''}로 변경되었습니다.\nTicket name has been changed to ${name}.`)
+    //                 .setTimestamp()
+    //         ]
+    //     });
+    // } catch(e) {}
 
     return interaction.reply(
         lang.langByLangName(interaction.dbUser.lang, 'TICKET_NAME_CHANGED')
