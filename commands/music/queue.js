ff6c0df33ae7cf6d6c797f98cb75065b1fc7a56b
Hyunsoo, Chung <admin@hyonsu.com>
2021-11-19 11:29:12 +0900

    chore: queue 1024 length limit

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -24,7 +24,7 @@ module.exports = {
                         .replace('{servername}', interaction.guild.name)
                     )
                     .addField(lang.langByLangName(interaction.dbUser.lang, 'MUSIC_QUEUE_NOW_PLAYING'), `${queue[0].title} (Added by ${interaction.client.users.cache.get(queue[0].createdUser)?.username || 'Unknown User'})`)
-                    .addField(lang.langByLangName(interaction.dbUser.lang, 'MUSIC_QUEUE_QUEUE'), queue.length === 1 ? lang.langByLangName(interaction.dbUser.lang, 'MUSIC_QUEUE_NO_QUEUE') : queue.slice(1).map((song, i) => `**${i + 1}.** ${song.title} (Added by ${interaction.client.users.cache.get(song.createdUser)?.username || 'Unknown User'})`).join('\n'))
+                    .addField(lang.langByLangName(interaction.dbUser.lang, 'MUSIC_QUEUE_QUEUE'), (queue.length === 1 ? lang.langByLangName(interaction.dbUser.lang, 'MUSIC_QUEUE_NO_QUEUE') : queue.slice(1).map((song, i) => `**${i + 1}.** ${song.title} (Added by ${interaction.client.users.cache.get(song.createdUser)?.username || 'Unknown User'})`).join('\n')).substring(0, 1024))
             ]
         });
     }
