85cb1a6735b989f529f825d4062351403aecf594
Hyunsoo, Chung <admin@hyonsu.com>
2022-01-04 00:15:17 +0900

    chore: use embed for features list

--- commands/features/list.js
+++ commands/features/list.js
@@ -1,3 +1,5 @@
+const { MessageEmbed } = require('discord.js');
+
 const main = require('../../main');
 
 const Guild = require('../../schemas/guild');
@@ -6,5 +8,13 @@ module.exports.commandHandler = async interaction => {
     const guild = await Guild.findOne({ id: interaction.guild.id });
     const groups = main.getGroups();
 
-    return interaction.reply(groups.map(g => `${guild.features.includes(g) ? '✅' : '❌'} ${g}`).join('\n'));
+    return interaction.reply({
+        embeds: [
+            new MessageEmbed()
+                .setColor('#349eeb')
+                .setTitle('Features')
+                .setDescription(groups.map(g => `${guild.features.includes(g) ? '✅' : '❌'} ${g}`).join('\n'))
+                .setTimestamp()
+        ]
+    });
 }
