c0cdfa787dd392f32cb359df073b4c521fe914af
정현수 <34962071+wjdgustn@users.noreply.github.com>
2022-02-15 08:51:35 +0900

    chore: ephemeral voice channel warning

--- buttonHandler/addQueue.js
+++ buttonHandler/addQueue.js
@@ -4,16 +4,19 @@ const lang = require('../lang');
 const music = require('../music');
 
 module.exports = async interaction => {
-    await interaction.deferReply();
-
     const params = interaction.customId.split('_');
-    if(params.length < 2) return interaction.editReply('invalid button');
+    if(params.length < 2) return interaction.reply('invalid button');
 
     const videoID = params.slice(1).join('_');
 
     if(!interaction.guild.me.voice.channel) {
         const voiceChannel = interaction.member.voice.channel;
-        if(!voiceChannel) return interaction.editReply(lang.langByLangName(interaction.dbUser.lang, 'MUSIC_NEED_TO_CONNECTED_VOICE_CHANNEL'));
+        if(!voiceChannel) return interaction.reply({
+            content: lang.langByLangName(interaction.dbUser.lang, 'MUSIC_NEED_TO_CONNECTED_VOICE_CHANNEL')),
+            ephemeral: true
+        }
+
+        await interaction.deferReply();
 
         await music.connect(voiceChannel, interaction.channel);
         await music.start(interaction.guild);
